b23a01533c4995cfbcf218468767f4bbd4673f89
ahmadhusain0205 <ahmad.husain.ardiansyah11@gmail.com>
2022-07-01 15:16:07 +0700

    create button cari di beranda

--- application/views/Beranda/Beranda.php
+++ application/views/Beranda/Beranda.php
@@ -1,44 +1,82 @@
-<section id="beranda_1" class="mb-4">
-     <div class="row">
-          <div class="col-6 offset-6">
-               <div class="row">
-                    <div class="col">
-                         <div class="form-group">
-                              <label for="Tahun_anggaran">Tahun Anggaran</label>
-                              <input type="date" class="form-control" id="thn_anggaran" name="thn_anggaran" value="<?= date('Y-m-d'); ?>">
+<form method="POST" id="formberanda">
+     <section id="beranda_1" class="mb-4">
+          <div class="row">
+               <div class="col-6 offset-6">
+                    <div class="row">
+                         <div class="col">
+                              <div class="form-group">
+                                   <label for="Tahun_anggaran">Tahun Anggaran</label>
+                                   <input type="date" class="form-control" id="thn_anggaran" name="thn_anggaran" value="<?= date('Y-m-d'); ?>">
+                              </div>
+                         </div>
+                         <div class="col">
+                              <div class="form-group">
+                                   <label for="Jenis_anggaran">Jenis Anggaran</label>
+                                   <select name="jns_anggaran" id="jns_anggaran" class="form-control">
+                                        <option value="">-- Pilih --</option>
+                                   </select>
+                              </div>
+                         </div>
+                         <div class="col my-auto">
+                              <button class="btn btn-primary" style="width: 100%; margin-top: 10px;" type="button" onclick="cari()">Cari</button>
                          </div>
                     </div>
-                    <div class="col">
-                         <div class="form-group">
-                              <label for="Jenis_anggaran">Jenis Anggaran</label>
-                              <select name="jns_anggaran" id="jns_anggaran" class="form-control">
-                                   <option value="">-- Pilih --</option>
-                              </select>
+               </div>
+          </div>
+     </section>
+     <section id="beranda_2" class="mb-4">
+          <div class="row">
+               <div class="col">
+                    <div class="card">
+                         <div class="card-body">
+                              <div class="h1"><?= strtoupper($judul); ?></div>
                          </div>
                     </div>
                </div>
           </div>
-     </div>
-</section>
-<section id="beranda_2" class="mb-4">
-     <div class="row">
-          <div class="col">
-               <div class="card">
-                    <div class="card-body">
-                         <div class="h1"><?= strtoupper($judul); ?></div>
+     </section>
+     <section id="beranda_3" class="mb-4">
+          <div class="row">
+               <div class="col">
+                    <div class="card">
+                         <div class="card-body text-center">
+                              <img src="<?= base_url('assets/img/logo-bos.jpg'); ?>" class="rounded" style="width: 500px;">
+                         </div>
                     </div>
                </div>
           </div>
-     </div>
-</section>
-<section id="beranda_3" class="mb-4">
-     <div class="row">
-          <div class="col">
-               <div class="card">
-                    <div class="card-body text-center">
-                         <img src="<?= base_url('assets/img/logo-bos.jpg'); ?>" class="rounded" style="width: 500px;">
+     </section>
+
+     <section id="beranda_4" class="mb-4">
+          <div class="row">
+               <div class="col">
+                    <div class="card">
+                         <div class="card-body">
+                              <div class="h5">DAFTAR</div>
+                              <hr>
+                              <div class="table-responsive">
+                                   <table class="table table-striped table-bordered table-hover">
+                                        <thead>
+                                             <tr>
+                                                  <th width="1%">No</th>
+                                                  <th>asd</th>
+                                             </tr>
+                                        </thead>
+                                   </table>
+                              </div>
+                         </div>
                     </div>
                </div>
           </div>
-     </div>
-</section>
+     </section>
+</form>
+
+<script>
+     $('#beranda_4').hide();
+
+     function cari() {
+          $('#beranda_2').hide('200');
+          $('#beranda_3').hide('200');
+          $('#beranda_4').show('200');
+     }
+</script>
