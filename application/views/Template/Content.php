7abaad648abd934b492ae37734523221abf01db7
ahmadhusain0205 <ahmad.husain.ardiansyah11@gmail.com>
2022-07-08 01:55:57 +0700

    add nama sd in topbar

--- application/views/Template/Content.php
+++ application/views/Template/Content.php
@@ -170,6 +170,7 @@
                                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                                         <i class="fa fa-bars"></i>
                                    </button>
+                                   <div class="h5 font-weight-bold text-primary">SD NEGERI NGADIPURO</div>
                                    <ul class="navbar-nav ml-auto">
                                         <li class="nav-item dropdown no-arrow mx-1">
                                              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
